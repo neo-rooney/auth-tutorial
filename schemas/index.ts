5acfbd39de0464c3adbc9017c6d84dfba7e7dcb4
rooney <bch3454@gmail.com>
2024-07-23 15:31:18 +0900

    feat: zod 테스트

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,10 +1,15 @@
 import * as z from "zod";
 
 export const LoginSchema = z.object({
-  email: z.string().email({
-    message: "Email is required",
-  }),
+  email: z
+    .string()
+    .min(1, {
+      message: "이메일 주소를 입력해주세요.",
+    })
+    .email({
+      message: "이메일 주소를 확인해주세요.",
+    }),
   password: z.string().min(1, {
-    message: "Password is required",
+    message: "패스워드를 입력해주세요.",
   }),
 });
